da5ef6af2198efb3a1dbbdc2862959b4684fc491
Mitsudrix <130903108+Mitsudrix@users.noreply.github.com>
2023-07-13 13:10:11 +0300

    Update DiamondUI.lua

--- DiamondUI.lua
+++ DiamondUI.lua
@@ -1215,7 +1215,7 @@ do
             Converted["_Main"].BackgroundColor3 = Color3.fromRGB(18.000000827014446, 18.000000827014446, 18.000000827014446)
             Converted["_Main"].BackgroundTransparency = 1
             Converted["_Main"].Position = UDim2.new(0.5, -320, 0.5, -219)
-            Converted["_Main"].Size = UDim2.new(0, 480, 0, 550)
+            Converted["_Main"].Size = UDim2.new(0, 480, 0, 800)
             Converted["_Main"].ZIndex = 100
             Converted["_Main"].Name = "Main"
             Converted["_Main"].Parent = Converted["_Atlas"]
